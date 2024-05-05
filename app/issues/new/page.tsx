0c4dc5d17968748379e80852197bfdb2d112a298
Kris Bailey <krisbailey7676@gmail.com>
2024-05-05 15:48:33 -0400

    Handle server error.  Handle form submission.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,50 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+// import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const SimpleMDE = dynamic(
+  () =>
+    import("../../../node_modules/react-simplemde-editor/dist/SimpleMdeReact"),
+  {
+    ssr: false,
+  }
+);
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")} // register function returns 4 props, so need to use spread operator
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
